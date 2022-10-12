407a821863d3c8d2fb9ef7cc175f28490686e1bf
Zachary Piontek <zakpiontek@gmail.com>
2022-10-11 19:19:16 -0500

    added fetch yelp handler

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,8 +2,27 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
-  // add code here to fetch data from yelp API
-  // be sure to include the parameters from event.queryStringParameters
+  const zip = event.queryStringParameters;
+
+  try {
+    const resp = await fetch(`https://api.yelp.com/v3/businesses/search/location=${zip}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+      },
+    });
+    const data = await resp.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.businesses),
+    };
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data' }),
+    };
+  }
 };
 
 module.exports = { handler };
